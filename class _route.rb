c39cb5bd66449a6fd5fbbfdbf480b23275493577
kristinashinkarenco <squirrelonair@gmail.com>
2024-03-19 22:47:30 +0300

    19_03

---
+++ class _route.rb
@@ -0,0 +1,22 @@
+class Route
+
+  attr_reader: stations
+	
+	def initialize(start_station, end_station)
+    @stations = [start_station, end_station]
+  end
+
+  def add_station(station) 
+    @stations.insert(-2, station)
+  end
+
+  def remove_station(station)
+  	if station != @stations.first && station != @stations.last
+  		 stations.delete(station)
+  end	
+
+  def show_stations
+  	puts "Stations on the route:" @station.each { |station| puts station.name }
+  end	
+
+end	
---
+++ class _train.rb
@@ -0,0 +1,57 @@
+lass Train
+
+	attr_reader :number, :type, :carriages, :current_speed, :current_station
+
+	def initialize( number, type, carriages)
+		@number = number
+		@type = type
+		@carriages = carriages
+		@current_speed = 0
+		@current_station_index = nil
+	end
+	
+	def speed_up(number)
+    @current_speed += number
+  end
+
+  def brake(number)
+    while @current_speed > 0
+      @current_speed -= number
+
+      @current_speed = 0 if @current_speed < 0
+    end  
+
+  def add_carriage
+    @carriages += 1 if @current_speed = 0
+  end
+
+  def remove_carriage
+    @carriages -= 1 if @current_speed = 0 
+  end
+
+  def assign_route(route)
+    @route = route
+    @current_station_index = 0
+    @current_station = @route.stations[@current_station_index]
+    @current_station.arrive_train(self)
+  end
+
+  def move_forward
+    return unless @route && @current_station_index < @route.stations.size - 1
+
+    @current_station.depart_train(self)
+    @current_station_index += 1
+    @current_station = @route.stations[@current_station_index]
+    @current_station.arrive_train(self)
+  end
+
+  def move_backward
+    return unless @route && @current_station_index > 0
+
+    @current_station.depart_train(self)
+    @current_station_index -= 1
+    @current_station = @route.stations[@current_station_index]
+    @current_station.arrive_train(self)
+  end
+  
+end
