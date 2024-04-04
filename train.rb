bcdd2cd74ecdd52efebce9f81bfd364e2db5616b
kristinashinkarenco <squirrelonair@gmail.com>
2024-04-04 21:32:21 +0300

    fix_irb_scold

--- train.rb
+++ train.rb
@@ -1,7 +1,7 @@
 сlass Train
   attr_reader :number, :type, :carriages, :current_speed, :current_station
 
-  def initialize( number, type, carriages)
+  def initialize(number, type, carriages)
     @number = number
     @type = type
     @carriages = carriages
@@ -20,16 +20,16 @@
   end  
 
   def add_carriage
-    @carriages += 1 if @current_speed = 0
+    @carriages += 1 if @current_speed == 0
   end
 
   def remove_carriage
-    @carriages -= 1 if @current_speed = 0 
+    @carriages -= 1 if @current_speed == 0 
   end
 
   def assign_route(route)
     @route = route
-    @current_station_index = 0
+    @current_station_index == 0
     @current_station = @route.stations[@current_station_index]
     @current_station.arrive_train(self)
   end
