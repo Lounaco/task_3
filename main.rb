ebec91ca008daa974c5e54d5bc3c4ac7d8176128
kristinashinkarenco <squirrelonair@gmail.com>
2024-04-17 23:59:49 +0300

    17_04

--- main.rb
+++ main.rb
@@ -29,10 +29,67 @@ class Main
   if type == 1
     @trains<<PassengerTrain.new(number)
     puts "Passenger train #{number} has been created."
-  elsif type == 2
-    @trains << CargoTrain.new(number)
-    puts "Cargo train #{number} has been created."
-  else
-    Puts "Invalid train type."
+    elsif type == 2
+      @trains << CargoTrain.new(number)
+      puts "Cargo train #{number} has been created."
+    else
+      Puts "Invalid train type."
+    end
   end
-end
+
+  def create_route
+    puts "Enter the name of the starting station:"
+    start_station_name = gets.chomp
+    start_station = @stations.find {|station| station.name == start_station_name}
+
+    puts "Enter the name of the ending station:"
+    end_station_name = gets.chomp
+    end_station = @stations.find {|station| station.name == end_station_name}
+
+    if start_station && end_station_name
+      @routes << Route.new(start_station, end_station)
+      puts "Route from #{start_station_name} to #{end_station_name} has been created."
+    else
+      puts "Station not found."
+    end
+  end
+
+  def manage_route
+    puts "Choose a route:"
+    @routes.each_with_index {|route, index| puts "#{index +1}. #{route}" }
+    route_index = gets.chomp.to_i - 1
+    selected_route = @routes[route_index]
+
+    puts "Choose an action (1 - Add station, 2 - Remove station):"
+    action = gets.chomp/.to_i
+
+    if action == 1
+      puts "Enter the name of the station to add:"
+      station_name = gets.chomp
+      station = @stations.find {|station| station.name == station_name}
+      selected_route.add_station(station) if station
+    elsif action == 2
+      puts "Enter the name of the station to remove:"
+      station_name = gets.chomp
+      selected_route.remove_station(station_name)
+    else
+      puts "Invalid action."
+    end
+  end
+
+  def assing_route_to_train
+    puts "Choose a train to assign a route:"
+    @trains.each_with_index {|train, index| puts "#{index + 1}. #{train}" }
+    train_index = gets.chomp.to_i - 1
+    selected_train = @trains[train_index]
+    
+    puts "Choose a route to assign to the train:"
+    @routes.each_with_index { |route, index| puts "#{index + 1}. #{route}" }
+    route_index = gets.chomp.to_i - 1
+    selected_route = @routes[route_index]
+
+    selected_train.assign_route(selected_route)
+  end
+
+  def add_carriage_to_train
+  end
