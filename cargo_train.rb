40c56b7f18d2c05636fe98502bf02ff6631a77d4
kristinashinkarenco <squirrelonair@gmail.com>
2024-06-10 13:50:56 +0300

    10_10/1

--- cargo_train.rb
+++ cargo_train.rb
@@ -5,4 +5,5 @@ class CargoTrain < Train
     super(number, type)
     @type = :Cargo
   end
+
 end
--- route.rb
+++ route.rb
@@ -5,6 +5,7 @@ class Route
 	
   def initialize(start_station, end_station)
     @stations = [start_station, end_station]
+    validate_data
   end
 
   def add_station(station) 
@@ -21,6 +22,21 @@ class Route
     puts "Stations on the route:"
     @station.each { |station| puts station.name }
   end
+
+  def valid?
+    validate_data
+  end
+
+  private
+
+  def validate_data
+    raise "Route must have at least two stations"
+unless @stations.size >= 2
+    raise "Start and end stations must be different"
+if @stations.first == @stations.last
+
+    true
+  end     
 end
 
 
