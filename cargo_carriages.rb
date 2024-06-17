18168977b677a5ce07ac97b8cdc8a759872722a7
kristinashinkarenco <squirrelonair@gmail.com>
2024-06-17 16:31:41 +0300

    17_06/2

--- cargo_carriages.rb
+++ cargo_carriages.rb
@@ -2,9 +2,28 @@
 class CargoCarriage
   include Manufacturer
   
-  attr_accessor :type
+  attr_accessor :type, total_volume, :occupied_volume
   
-  def initialize
+  def initialize(total_volume)
     @type = :cargo_carriage
+    @total_volume = total_volume
+    @occupied_volume = 0
   end
+
+  def occupy_volume(volume)
+    if @occupied_volume + volume <= @total_volume
+      @occupied_volume += volume
+    else
+      puts "Not enough avaliable volume."
+    end
+  end
+
+  def occupied_volume_count
+      @occupied_volume
+  end
+
+  def free_volume_count
+    @total_volume - @occupied_volume
+  end  
+
 end
--- passenger_carriages.rb
+++ passenger_carriages.rb
@@ -3,9 +3,28 @@
 class PassengerCarriage
   include Manufacturer
   
-  attr_accessor :type
+  attr_accessor :type, :total_seats, :occupied_seats
   
-  def initialize
+  def initialize(total_seats)
     @type = :passanger_carriage
+    @total_seats = total_seats
+    @occupied_seats = 0
   end
+
+  def occupy_seat
+    if @occupied_seats < @total_seats
+      @occupied_seats += 1
+    else
+      puts "All seats are occupied"
+    end
+  end
+
+  def occupied_seats_count
+    @occupied_seats
+  end
+  
+  def free_seats_count
+    @total_seats - @occupied_seats
+  end    
+
 end
